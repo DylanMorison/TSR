dbf9c36422609527eb00b72dad6ac52ba466bb87
Dylan Morison <dylan@g.morison.org>
2021-04-13 16:05:22 -0700

    cleaning up before branch change

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import Head from "./components/Head";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import ReducerButtons from "./components/ReducerButtons";
-import "./App.css";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<Head title="hello" isActive={true} />
-			<Input />
-			<Button
-				onClick={e => {
-					console.log("hit");
-
-					e.preventDefault();
-				}}
-			>
-				Hello FC
-			</Button>
-			<ReducerButtons />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="App" style={{ fontFamily: "monospace" }}>
+			test
 		</div>
 	);
 };
